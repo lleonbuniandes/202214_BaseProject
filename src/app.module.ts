ebe13efef5906bcbeb24917cd3fb8e0d9cf76309
Luis Alfredo Leon <l.leonb@uniandes.edu.co>
2022-09-18 18:36:47 -0500

    Creacion bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
 import { SupermercadoModule } from './supermercado/supermercado.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad/ciudad.entity';
+import { SupermercadoEntity } from './supermercado/supermercado.entity';
 
 @Module({
-  imports: [CiudadModule, SupermercadoModule],
+  imports: [CiudadModule, SupermercadoModule,
+     TypeOrmModule.forRoot({
+       type: 'postgres',
+       host: 'localhost',
+       port: 5432,
+       username: 'postgres',
+       password: 'postgres',
+       database: 'parcial',
+       entities: [CiudadEntity, SupermercadoEntity],
+       dropSchema: true,
+       synchronize: true,
+       keepConnectionAlive: true
+     }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
